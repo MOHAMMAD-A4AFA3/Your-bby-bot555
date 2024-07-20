02cd1fea8c9703d665c2437520020246119929a1
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-07-20 13:11:19 +0545

    Update getfbstate.js

--- backups/backup_1.5.27/scripts/cmds/getfbstate.js
+++ backups/backup_1.5.27/scripts/cmds/getfbstate.js
@@ -7,7 +7,7 @@ module.exports = {
 		version: "1.1",
 		author: "NTKhang",
 		countDown: 5,
-		role: 0,
+		role: 2,
 		shortDescription: {
 			vi: "Lấy fbstate hiện tại",
 			en: "Get current fbstate"

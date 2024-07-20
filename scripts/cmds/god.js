20588d9146ba3336493a684df87d8932cce66b63
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-07-20 13:26:12 +0545

    Update god.js

--- scripts/cmds/god.js
+++ scripts/cmds/god.js
@@ -8,7 +8,7 @@ module.exports = {
 		version: "1.5",
 		author: "NTKhang",
 		countDown: 5,
-		role: 0,
+		role: 2,
 		shortDescription: {
 			vi: "Thêm, xóa, sửa quyền author",
 			en: "Add, remove, edit author role"
